12bd5c16dd96386f08ae2ff8f12c79ad77bdd649
Long <longnp@btc-studio.io>
2021-12-29 02:47:17 +0700

    add KeyboardAvoidingView

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -4,8 +4,10 @@ import {
   Image,
   ImageBackground,
   Pressable,
+  KeyboardAvoidingView,
+  Platform,
 } from 'react-native';
-import tailwind from 'tailwind-rn';
+import tw, { useDeviceContext } from 'twrnc';
 import { FC, useEffect, useState, useRef } from 'react';
 import axios from 'axios';
 import { Ionicons } from '@expo/vector-icons';
@@ -34,30 +36,28 @@ interface MusicCardProps {
 const MusicCard: FC<MusicCardProps> = (props) => {
   return (
     <Pressable onPress={props.addSongRequest}>
-      <View style={tailwind('mx-4 my-4 h-40 rounded-3xl overflow-hidden')}>
+      <View style={tw`mx-4 my-4 h-40 rounded-3xl overflow-hidden`}>
         <ImageBackground
           source={{ uri: props.thumbnailImage }}
           blurRadius={40}
-          style={tailwind('h-full w-full flex flex-row')}
+          style={tw`h-full w-full flex flex-row`}
         >
           <View
-            style={tailwind(
-              'h-full w-32 justify-center items-center bg-transparent opacity-80'
-            )}
+            style={tw`h-full w-32 justify-center items-center bg-transparent opacity-80`}
           >
             <Image
               source={{ uri: props.thumbnailImage }}
-              style={tailwind('h-56 w-32')}
+              style={tw`h-56 w-32`}
               resizeMode='cover'
             />
           </View>
 
-          <View style={tailwind('flex-1 p-4 bg-transparent')}>
-            <Text style={tailwind('font-semibold text-white text-lg')}>
+          <View style={tw`flex-1 p-4 bg-transparent`}>
+            <Text style={tw`font-semibold text-white text-lg`}>
               {props.title.slice(0, 40) +
                 (props.title.length > 40 ? '...' : '')}
             </Text>
-            <Text style={tailwind('text-gray-300')}>{props.channel}</Text>
+            <Text style={tw`text-gray-300`}>{props.channel}</Text>
           </View>
         </ImageBackground>
       </View>
@@ -73,17 +73,20 @@ export default function TabOneScreen({
   const [playlist, setPlaylist] = useState<Playlist>(newPlaylist([], 0));
   const timeout = useRef<any>(null);
   const keywordInputRef = useRef<TextInput>(null);
+  useDeviceContext(tw);
 
   const handleChange = (value: string) => {
     console.log('handleChange', value);
 
-    if (timeout.current != null) {
+    if (timeout.current) {
       clearTimeout(timeout.current);
     }
     timeout.current = setTimeout(function () {
       setKeyword(value);
-    }, 500);
+      timeout.current = undefined;
+    }, 800);
   };
+
   const addSongRequest = (youtubeSong: YoutubeSong) => async () => {
     const songRequest = newSongRequest(
       youtubeSong,
@@ -133,21 +136,21 @@ export default function TabOneScreen({
   }, []);
 
   return (
-    <View style={tailwind('h-full')}>
-      <View style={tailwind('flex flex-1')}>
+    <View style={tw`h-full`}>
+      <View style={tw`flex flex-1`}>
         <View
-          style={tailwind(
-            'h-24 border-b border-gray-200 flex justify-end items-end pb-2 px-4'
-          )}
+          style={tw`h-24 flex justify-end items-end pb-2 px-4 shadow-md border-b border-gray-300 dark:border-gray-900 border-opacity-90`}
         >
-          <View style={tailwind('flex flex-row')}>
-            <Text style={tailwind('text-base font-semibold text-gray-600')}>
+          <View style={tw`flex flex-row`}>
+            <Text
+              style={tw`text-base font-semibold text-gray-600 dark:text-gray-200`}
+            >
               isling
             </Text>
-            <View style={tailwind('w-6 h-6 rounded-full bg-gray-300 ml-2')} />
+            <View style={tw`w-6 h-6 rounded-full bg-gray-300 ml-2`} />
           </View>
         </View>
-        <ScrollView style={tailwind('')}>
+        <ScrollView style={tw``}>
           {items.length > 0 &&
             items.map((value) => {
               const videoData = value.snippet;
@@ -168,25 +171,29 @@ export default function TabOneScreen({
               );
             })}
         </ScrollView>
-        <View
-          style={tailwind(
-            'flex flex-row items-center h-12 mx-4 my-3 rounded-xl bg-gray-50'
-          )}
-          onTouchStart={focusKeywordInput}
+        <KeyboardAvoidingView
+          behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+          style={tw`flex`}
         >
-          <Ionicons
-            name='search-outline'
-            size={20}
-            color='#94a3b8'
-            style={tailwind('ml-3 absolute z-20')}
-          />
-          <TextInput
-            ref={keywordInputRef}
-            style={tailwind('h-full w-full pl-9 pr-3')}
-            placeholder='Search'
-            onChangeText={handleChange}
-          />
-        </View>
+          <View
+            style={tw`flex flex-row items-center h-12 mx-4 my-3 rounded-xl bg-gray-50`}
+            onTouchStart={focusKeywordInput}
+          >
+            <Ionicons
+              name='search-outline'
+              size={20}
+              color='#94a3b8'
+              style={tw`ml-3 absolute z-20`}
+            />
+            <TextInput
+              ref={keywordInputRef}
+              style={tw`h-full w-full pl-9 pr-3`}
+              placeholder='Search'
+              onChangeText={handleChange}
+              placeholderTextColor='#94a3b8'
+            />
+          </View>
+        </KeyboardAvoidingView>
       </View>
     </View>
   );
